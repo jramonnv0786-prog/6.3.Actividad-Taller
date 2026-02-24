eb47cfc614fc4024fd694f2ff91f8633e07e7900
Jose Ramon Neira Vega <jramonnv0786@gmail.com>
2026-02-24 11:37:57 +0100

    Creada clase Reparación

---
+++ Taller.java/Reparación.java
@@ -0,0 +1,13 @@
+import java.util.Date;
+
+public class Reparacion {
+    private Date fecha;
+    private String descripcion;
+    private double coste;
+
+    public Reparacion(String descripcion, double coste) {
+        this.fecha = new Date();
+        this.descripcion = descripcion;
+        this.coste = coste;
+    }
+}
